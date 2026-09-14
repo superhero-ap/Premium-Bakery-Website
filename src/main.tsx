8e43f849ca783180b772c88237e4c5898787ef84
superhero-ap <superhero845303@gmail.com>
2026-09-14 16:25:03 +0530

    refactor: use customer account host component

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode, useEffect, useState } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './styles.css'
@@ -7,24 +7,14 @@ import './cart-ui.css'
 import './account.css'
 import App from './App'
 import CartCountSync from './components/CartCountSync'
-import CustomerPortal from './components/CustomerPortal'
-
-function CustomerAccount() {
-  const [open, setOpen] = useState(false)
-  useEffect(() => {
-    const openAccount = () => setOpen(true)
-    window.addEventListener('open-customer-account', openAccount)
-    return () => window.removeEventListener('open-customer-account', openAccount)
-  }, [])
-  return open ? <CustomerPortal onClose={() => setOpen(false)} /> : null
-}
+import CustomerAccountHost from './components/CustomerAccountHost'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
       <CartCountSync />
-      <CustomerAccount />
+      <CustomerAccountHost />
     </BrowserRouter>
   </StrictMode>,
 )
