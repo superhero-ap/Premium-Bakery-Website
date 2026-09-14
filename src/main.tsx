3e053cd20398c6f605d49ecf548e419fcc3b3c04
superhero-ap <superhero845303@gmail.com>
2026-09-14 16:23:14 +0530

    feat: expose customer account portal

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './styles.css'
@@ -6,12 +6,22 @@ import './admin.css'
 import './cart-ui.css'
 import App from './App'
 import CartCountSync from './components/CartCountSync'
+import CustomerPortal from './components/CustomerPortal'
+
+function CustomerAccount() {
+  const [open, setOpen] = useState(false)
+  return <>
+    <button className="account-trigger" type="button" onClick={() => setOpen(true)} aria-label="Open customer account">Account</button>
+    {open && <CustomerPortal onClose={() => setOpen(false)} />}
+  </>
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
       <CartCountSync />
+      <CustomerAccount />
     </BrowserRouter>
   </StrictMode>,
 )
